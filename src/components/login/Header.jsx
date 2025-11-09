cb0a9ab40267d267934c94f885a6c94c1d55cfce
promahbubul <mahbublalam500@gmail.com>
2025-11-09 14:36:53 +0600

    [ADD]: Login page - Header component  create

---
+++ src/components/login/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <div>
+      <div className="">
+        <img
+          src="https://lineone.piniastudio.com/images/app-logo.svg"
+          alt=""
+          className="w-20 mx-auto text-center"
+        />
+      </div>
+      <h2 className="text-[#c2c9d6] mt-5 text-[26px] font-bold text-center">
+        Welcome Back
+      </h2>
+      <p className="text-[#697a9b]   text-base text-center ">
+        Please sign in to continue
+      </p>
+    </div>
+  );
+};
+export default Header;
