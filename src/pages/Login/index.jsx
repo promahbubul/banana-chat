73a8ec3a11050c63c9b7e5d889e1475f20fa98da
promahbubul <mahbublalam500@gmail.com>
2025-11-09 15:28:25 +0600

    [ADD]: login page - privacy policy

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,6 +7,11 @@ const Login = () => {
       <div className="max-w-md  w-full">
         <Header />
         <LoginForm />
+        <div className="text-[#697a9b] flex flex-row items-center justify-center mt-5 gap-5">
+          <p className="">Privacy Notice </p>
+          <div className="w-px h-4 bg-[#697a9b]"></div>
+          <p className="">Term of service</p>
+        </div>
       </div>
     </div>
   );
